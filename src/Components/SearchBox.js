fd7870c7cd96daa2c4e42b60b1d467d9cd42d685
Glitch (ossified-watery-end) <none>
2020-05-22 09:59:13 +0000

    🐘🛰 add a function checking input string if it is number phone.

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,62 +1,74 @@
-import React from 'react';
-import './SearchBox.css';
-import classnames from 'classnames';
+import React from "react";
+import "./SearchBox.css";
+import classnames from "classnames";
 
 class SearchBox extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            isFocused: false,
-          overLength: false
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      isFocused: false,
+      overLength: false,
+      notPhoneNumber: false
+    };
+  }
 
-    changeState() {
-        const { isFocused } = this.state;
-        if (isFocused) {
-            this.setState({isFocused: !isFocused});
-        } else {
-            this.setState({isFocused: !isFocused});
-        }
+  changeState() {
+    const { isFocused } = this.state;
+    if (isFocused) {
+      this.setState({ isFocused: !isFocused });
+    } else {
+      this.setState({ isFocused: !isFocused });
     }
-  
-  changeStateOverLength (length) {
-        const { overLength } = this.state;
-        if (length > 10) {
-            this.setState({overLength: true});
-        } else {
-            this.setState({overLength: false});
-        }
-    }
+  }
 
-    checkOverLength (id, cb) {
-        const input = document.getElementById(id);
-        // console.log(input.value);
-        cb(input.value.length);
+  changeStateOverLength(length) {
+    const { overLength } = this.state;
+    if (length > 10) {
+      this.setState({ overLength: true });
+    } else {
+      this.setState({ overLength: false });
     }
+  }
 
-    render() {
-        const { isFocused } = this.state;
+  checkOverLength(id, cb) {
+    const input = document.getElementById(id);
+    // console.log(input.value);
+    cb(input.value.length);
+  }
+  checkPhoneNumber(id) {
+    const input = document.getElementById(id);
+    const rePhoneNumber = new RegExp(/^090[0-9]{7}$/, "g");
+    this.setState({
+      notPhoneNumber: !rePhoneNumber.test(input.value)
+    });
+  }
+  render() {
+    const { isFocused } = this.state;
 
-        return (
-            <div className={classnames('search-box')}>
-                <input 
-                    onFocus={this.changeState.bind(this)} 
-                    onBlur={this.changeState.bind(this)} 
-                  onKeyUp={this.checkOverLength.bind(null,'input', this.changeStateOverLength.bind(this))}
-                    id="input"
-                    type="text" 
-                  className={classnames('search-input', {'border-red': this.state.overLength})}
-                    placeholder="Type something to search ..." 
-                />
-                <div className={
-                        classnames('search-button', {'hide': isFocused})
-                    }>
-                    <div class="fas fa-search"></div>
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className={classnames("search-box")}>
+        <input
+          onFocus={this.changeState.bind(this)}
+          onBlur={this.changeState.bind(this)}
+          onKeyUp={this.checkOverLength.bind(
+            null,
+            "input",
+            this.changeStateOverLength.bind(this)
+          )}
+          onChange={this.checkPhoneNumber.bind(this, "input")}
+          id="input"
+          type="text"
+          className={classnames("search-input", {
+            "border-red": this.state.overLength || this.state.notPhoneNumber
+          })}
+          placeholder="Type something to search ..."
+        />
+        <div className={classnames("search-button", { hide: isFocused })}>
+          <div class="fas fa-search"></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchBox;
