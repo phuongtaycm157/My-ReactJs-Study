0b066450081e910f9e63323a6ada33dd499a1a61
Glitch (brief-elastic-poinsettia) <none>
2020-05-26 08:45:08 +0000

    👣🐾 Update Modal component with props content

--- src/App.js
+++ src/App.js
@@ -1,159 +1,45 @@
 import React from 'react';
-// import ill_bg_todo_app from './Images/ill_bg_todo_app.svg';
+import logo from './logo.svg';
 import './App.css';
-import classnames from 'classnames';
-import Todo from './Components/Todo';
+import Modal from './components/Modal';
+
+const content = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      addStateDislay: false,
-      newContent: '',
-      wrongString: false,
-      todoList: [
-        // {id: 1, content: 'College', isCompletion: true},
-        // {id: 2, content: 'Workout', isCompletion: true},
-        // {id: 3, content: 'Working on Project', isCompletion: false},
-        // {id: 4, content: 'Doing Homework', isCompletion: false},
-      ]
+      hideModal: true
     };
-    this.changeCompletion = this.changeCompletion.bind(this);
-    this.onAddStateDislay = this.onAddStateDislay.bind(this);
-    this.offAddStateDislay = this.offAddStateDislay.bind(this);
-    this.updateTodoList = this.updateTodoList.bind(this);
-    this.saveContent = this.saveContent.bind(this);
-    this.onChangeValue = this.onChangeValue.bind(this);
+    this.showModal = this.showModal.bind(this);
+    this.hiddenModal = this.hiddenModal.bind(this); 
   }
 
-  changeCompletion(item) {
-    return (event) => {
-      const { todoList } = this.state;
-      const index = todoList.indexOf(item);
-      this.setState({
-        todoList: [
-          ...todoList.slice(0, index),
-          {id: item.id, content: item.content, isCompletion: !item.isCompletion},
-          ...todoList.slice(index+1)
-        ]
-      })
-    }
+  showModal() {
+    this.setState({hideModal: false});
   }
 
-  offAddStateDislay() {
-    this.setState({
-      addStateDislay: false
-    })
+  hiddenModal() {
+    this.setState({hideModal: true});
   }
-
-  onAddStateDislay() {
-    this.setState({
-      addStateDislay: true
-    })
-  }
-
-  updateTodoList(event) {
-    let content = this.state.newContent;
-    content = content.trim();
-    if(content.length === 0) {
-      this.setState({
-        wrongString: true
-      })
-      return;
-    }
-    this.setState({
-      todoList: [
-        ...this.state.todoList,
-        {id: this.state.todoList.length+1, content: content, isCompletion: false}
-      ],
-      newContent: '',
-      wrongString: false
-    })
-    this.offAddStateDislay();
-  }
-
-  saveContent(event) {
-    if (event.keyCode === 13){
-      this.updateTodoList();
-    }
-  }
-
-  onChangeValue(event) {
-    this.setState({
-      newContent: event.target.value
-    })
-  }
-
   render(){
-    const { todoList, newContent, wrongString } = this.state;
-    const todoListYes = todoList.filter(item => !item.isCompletion);
-    const todoListNo = todoList.filter(item => item.isCompletion);
     return (
       <div className="App">
-
-        <header>
-          <i className="fas fa-bars"></i>
-          <div className="title">DAILIST</div>
-        </header>
-
-        <div className={classnames("screen", {'none-display': this.state.addStateDislay})}>
-
-          <img 
-          alt="Illustration" 
-          className = {
-            classnames('illustration', {
-              'none-display': todoList.length > 0 ? true : false
-            })
-          }
-          src='https://cdn.glitch.com/4a661d5a-698d-4b41-8a22-a3d702fdef3e%2Fill_bg_todo_app.svg?v=1590236650989' />
-
-          <div className = {classnames('todo-list', {
-                            'none-display': todoList.length > 0 ? false : true
-                            })
-          }>
-
-            <div className={classnames("upcoming", {'none-display': todoListYes.length === 0})}>Upcoming</div>
-            {todoListYes.length > 0 && todoListYes.map(item => {
-              return <Todo 
-                      key={item.id} 
-                      todo={item}
-                      onClick={this.changeCompletion(item)}
-                      />
-            })}
-            <div className="h-space"></div>
-            <div className={classnames("upcoming", {'none-display': todoListNo.length === 0})}>Finished</div>
-            {todoListNo.length > 0 && todoListNo.map(item => {
-              return <Todo 
-                      key={item.id} 
-                      todo={item}
-                      onClick={this.changeCompletion(item)}
-                      />
-            })}
-          </div>
-        </div>
-
-        <div className={classnames('screen', {'none-display': !this.state.addStateDislay})}>
-          <i 
-          onClick={this.offAddStateDislay}
-          className="close fas fa-times"></i>
-          <div className="add-control">
-            <input 
-            className={classnames("add-input", {'wrong-string': wrongString})} 
-            placeholder="Write somethings here . . ." 
-            value={newContent}
-            onChange={this.onChangeValue}
-            onKeyUp={this.saveContent}
-            />
-            <button onClick={this.updateTodoList} className="add-button">ADD</button>
-          </div>
-        </div>
-
-        <footer>
-          <button onClick={this.onAddStateDislay}>
-            <i className="fas fa-plus"></i>
-          </button>
-        </footer>
-
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              <button onClick={this.showModal}> Open modal</button>
+            </p>
+            <a
+              className="App-link"
+              href="https://coders-x.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Yêu anh Thịnh
+            </a>
+          </header>
+    {!this.state.hideModal && <Modal content={content} hiddenModal={this.hiddenModal} /> }
       </div>
     );
   }
---
+++ src/components/Modal.js
@@ -0,0 +1,27 @@
+// 1. Code ra như design https://www.figma.com/file/M2Lh7XaEKWnds9SeEuJ1l0/B%C3%A0i-t%E1%BA%ADp-CSS-cho-entry-test?node-id=87%3A102
+// 2. Thêm prop `content` thay vì hard-code text content trong Modal
+import React from 'react';
+import classnames from 'classnames';
+// import Close from './close.svg';
+import './Modal.css';
+
+class Modal extends React.Component {
+  render() {
+    const { content } = this.props;
+    return (
+      <div className={classnames('modal-background', {'hide': this.props.hide})}>
+        <div className="modal">
+          <div className="title">This is a modal 1</div>
+            <p>{content}</p>
+          <div className="control-buttom">
+            <button onClick={this.props.hiddenModal} className="btn btn-sucess">Accept</button>
+            <button onClick={this.props.hiddenModal} className="btn btn-danger">Decline</button>
+          </div>
+          <img onClick={this.props.hiddenModal} alt="close" src='https://cdn.glitch.com/c6431814-047b-4458-9547-3b29b0cbdf43%2Fclose.svg?v=1590482449506' />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Modal;
